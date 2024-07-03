65c8bf0a205f216a00d15f779e80b5723a10eb03
changhyun-cra <hellow6044@gmail.com>
2024-07-03 14:53:29 +0900

    page42

--- Project4/baseball.cpp
+++ Project4/baseball.cpp
@@ -4,15 +4,33 @@ using namespace std;
 
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+	bool isDuplicatedNumber(const string guessNumber) {
+		if (guessNumber[0] == guessNumber[1] ||
+			guessNumber[1] == guessNumber[2] ||
+			guessNumber[0] == guessNumber[2]) {
+			return true;
+		}
+		return false;
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch>'9') {
-				throw invalid_argument("Must be number");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number");
+		}
+
+		if (isDuplicatedNumber(guessNumber)) {
+			throw invalid_argument("Must not have the same number");
 		}
 	}
+
+	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+	
 };
